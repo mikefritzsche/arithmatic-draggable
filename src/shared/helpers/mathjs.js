bb0dcb0cda29c540ae0f49304f75c63b5ee27721
Mike Fritzsche <mike@mikefritzsche.com>
2022-12-02 09:20:54 -0800

    update mathjs methods

--- src/shared/helpers/mathjs.js
+++ src/shared/helpers/mathjs.js
@@ -4,12 +4,53 @@ const config = { }
 const math = create(all, config)
 
 // let s0 = '10 + 20 * 2 * 0.75'
-let s1 = '((10+20)*2) * 0.75'
-// let s2 = '10 + 20'
+let s1 = '((20+30)*4) * 0.75'
+let s2 = '10 + 20'
 
 // helper(s0, false)
-helper(s1, true)
+const outputS1 = helper(s1, true)
+// const outputS2 = helper(s2, true)
 // helper(s2, false)
+// console.log(outputS1)
+// buildTree(math.parse(s1))
+console.log(s1)
+
+let nestLevel = 0
+// buildTree(math.parse(s1), 0, nestLevel)
+
+function buildTree(tree, position = 0, nestLevel = 0) {
+  tree.forEach((node, path, parent) => {
+    switch (node.type) {
+      case 'OperatorNode':
+        // console.log(node)
+        console.log(node.type)
+        console.log(node.value)
+        console.log(path)
+        console.log(parent)
+        // console.log([position, nestLevel])
+        break
+      case 'ConstantNode':
+        // console.log(node)
+        console.log(node.type)
+        console.log(node.value)
+        console.log(path)
+        console.log(parent)
+        // console.log([position, nestLevel])
+        break
+      case 'ParenthesisNode':
+        // console.log(node)
+        console.log(node.type)
+        console.log(node.content)
+        console.log(node.content.fn)
+        console.log(path)
+        console.log(parent)
+        // console.log([position, nestLevel])
+        buildTree(node.content.args, position)
+        break
+      default:
+    }
+  })
+}
 
 function helper(formulaString, traverse = false) {
   let parsedExpression = math.parse(formulaString)
@@ -19,30 +60,46 @@ function helper(formulaString, traverse = false) {
   // formulaString
 
   if (traverse) {
-    parsedExpression.forEach(item => {
-      // console.log(item)
-    })
-    return
+    // parsedExpression.forEach(item => {
+    //   // console.log(item)
+    // })
+    // return
+    let output = {
+      operation: '',
+      query: [],
+      operands: []
+    }
+    let position = 0
     parsedExpression.traverse(function (node, path, parent) {
       // console.log(node)
 
       switch (node.type) {
         case 'OperatorNode':
-          // console.log(node.type)
-          // console.log(node.op)
-          // console.log(path)
-          console.log(parent)
+          console.log(node.type)
+          console.log(node.op)
+          console.log(path)
+          console.log(node.fn)
+          if (path === null) {
+            output.operation = node.fn
+          }
+          // console.log(parent)
           break
         case 'ConstantNode':
-          // console.log(node.type)
-          // console.log(node.value)
-          // console.log(path)
+          console.log(node.type)
+          console.log(node.value)
+          console.log(path)
           console.log(parent)
+          output.operands.push({
+            position,
+            value_type: 'constant',
+            value: node.value
+          })
+          position = position + 1
           break
         case 'ParenthesisNode':
-          // console.log(node.type)
-          // console.log(path)
-          console.log(parent)
+          console.log(node.type)
+          console.log(path)
+          // console.log(parent)
           break
         default:
         // console.log(node.type)
@@ -50,6 +107,8 @@ function helper(formulaString, traverse = false) {
         // console.log(parent)
       }
     })
+
+    return output
   }
 }
 
